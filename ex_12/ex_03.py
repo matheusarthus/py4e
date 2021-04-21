724138119bfbe9da0292b34470b576568679bae4
matheusarthus <matheus.arthus@gmail.com>
2021-04-21 17:45:15 -0300

    ex_12-03

--- ex_12/ex_03.py
+++ ex_12/ex_03.py
@@ -3,22 +3,18 @@ import urllib.request
 url = input('Enter the URL of the web site: ')
 
 try:
-    host = url.split('/')[2]
+    fhand = urllib.request.urlopen(url)
 except:
-    print("URL format is incorrect.")
+    print("URL does not exist.")
     quit()
 
 count = 0
 
-while True:
-    data = mysock.recv(512)
-    if (len(data) < 1):
-        break
-    words = data.decode().split()
+for line in fhand:
+    words = line.decode().strip().split()
     for word in words:
         for character in word:
             count = count + 1
             if count < 3000: print(character)
-mysock.close()
 
 print("Total number of characters: ", count)
