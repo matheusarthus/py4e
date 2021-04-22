d62585788dd45a05b32f20aeeeab60356178ce5f
matheusarthus <matheus.arthus@gmail.com>
2021-04-22 08:25:45 -0300

    ex_12-05

---
+++ ex_12/ex_05.py
@@ -0,0 +1,23 @@
+import socket
+
+url = input('Enter the URL of the web site: ')
+
+try:
+    host = url.split('/')[2]
+except:
+    print("URL format is incorrect.")
+    quit()
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect((host, 80))
+cmd = 'GET ' + url + ' HTTP/1.0\r\n\r\n'
+mysock.send(cmd.encode())
+
+while True:
+    data = mysock.recv(512)
+    if (len(data) < 1):
+        break
+    decodedData = data.decode()
+    endHeader = decodedData.find("\r\n\r\n")
+    print(decodedData[endHeader + 1:])
+mysock.close()
